357d097972b0de962a9ec7e0af895110b0da62b0
Myron 19 <afful.myron@gmail.com>
2023-02-22 14:19:28 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print _putcahr
+ * is written by Myron19
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('p');
+_putchar('\n');
+return (0);
+}
