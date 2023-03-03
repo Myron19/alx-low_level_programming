4cbb0f15db4adbc1fa5daf8cbc33fa4be98c96bb
Myron 19 <afful.myron@gmail.com>
2023-03-02 16:11:36 -0800

    Task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strcmp - Function which compare two strings
+ *_strcmp - Function which compare two strings and
  * @s1: first string
  * @s2: second string
  * Return:
@@ -23,15 +23,19 @@ int _strcmp(char *s1, char *s2)
 			diff = s2[i];
 			break;
 		}
-		else if (s1[i] == '\0')
+		else if (s2[i] == '\0')
 		{
 			diff = s1[i];
 			break;
 		}
 		else if (s1[i] != s2[i])
 		{
-			else
-				i++;
+			diff = s1[i] - s2[i];
+			break;
 		}
-		return (diff);
+		else
+			i++;
+	}
+	return (diff);
 }
+
